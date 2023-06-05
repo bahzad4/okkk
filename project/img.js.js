208e4e381f5ede5dc65c60c642d47fdc96ab9f4f
bahzad4 <121199196+bahzad4@users.noreply.github.com>
2023-06-05 19:31:49 +0300

    Add files via upload

---
+++ project/img.js.js
@@ -0,0 +1,13 @@
+let index=0;
+bahzad();
+function  bahzad()
+{let i;
+    a=document.getElementsByClassName("a");
+  for( i=0;i<a.length;i++){
+    a[i].style.display="";
+   }
+index++;
+   if (index>a.length){index=1;}
+a[index-1].style.display="block";
+setTimeout(bahzad,100);
+}
